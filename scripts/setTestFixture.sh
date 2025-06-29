0b4b42adf471508a7a76f04126bf367d42d2672c
Yegor Bugayenko <yegor256@gmail.com>
2025-06-29 09:51:10 +0300

    #3 bash fix

--- scripts/setTestFixture.sh
+++ scripts/setTestFixture.sh
@@ -2,7 +2,8 @@
 
 # Uses fixed grammar for testing. If grammar is changed, the tests
 # need to change as well
-cp $(pwd)/testFixture/Eo.g4 $(pwd)/resources/Eo.g4
+
+cp "$(pwd)/testFixture/Eo.g4" "$(pwd)/resources/Eo.g4"
 
 npm run build-parser
 npm run compile
