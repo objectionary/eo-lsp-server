8f904f0ba6cf6caeb12dcec385af798c635bbe43
Yegor Bugayenko <yegor256@gmail.com>
2025-06-29 21:47:44 +0300

    #26: Refactor IndentationLexer methods to be static.

--- src/IndentationLexer.ts
+++ src/IndentationLexer.ts
@@ -12,7 +12,7 @@ export class IndentationLexer extends Lexer {
     private pendingTokens: Token[] = [];
     private lastPosition = 0;
     private indentStack: number[] = [0];
-    
+
     public static readonly TAB = 31;
     public static readonly UNTAB = 32;
 
@@ -26,7 +26,7 @@ export class IndentationLexer extends Lexer {
      * @param token - Token to check
      * @returns true if token is EOL
      */
-    private isEOL(token: Token): boolean {
+    private static isEOL(token: Token): boolean {
         return token.type === EoLexer.EOL;
     }
 
@@ -52,8 +52,8 @@ export class IndentationLexer extends Lexer {
      * @param text - EOL token text
      * @returns Indentation level (number of double spaces)
      */
-    private getIndentLevel(text: string): number {
-        const afterNewline = text.substring(text.indexOf('\n') + 1);
+    private static getIndentLevel(text: string): number {
+        const afterNewline = text.slice(text.indexOf("\n") + 1);
         return Math.floor(afterNewline.length / 2);
     }
 
@@ -67,13 +67,13 @@ export class IndentationLexer extends Lexer {
         }
 
         const token = this.wrapped.nextToken();
-        
-        if (this.isEOL(token) && token.type !== Token.EOF) {
-            const currentIndent = this.getIndentLevel(token.text || "");
+
+        if (IndentationLexer.isEOL(token) && token.type !== Token.EOF) {
+            const currentIndent = IndentationLexer.getIndentLevel(token.text || "");
             const previousIndent = this.indentStack[this.indentStack.length - 1];
-            
+
             this.pendingTokens.push(token);
-            
+
             if (currentIndent > previousIndent) {
                 this.indentStack.push(currentIndent);
                 const tabToken = this.createToken(
@@ -84,7 +84,7 @@ export class IndentationLexer extends Lexer {
                 );
                 this.pendingTokens.push(tabToken);
             } else if (currentIndent < previousIndent) {
-                while (this.indentStack.length > 1 && 
+                while (this.indentStack.length > 1 &&
                        this.indentStack[this.indentStack.length - 1] > currentIndent) {
                     this.indentStack.pop();
                     const untabToken = this.createToken(
@@ -96,11 +96,11 @@ export class IndentationLexer extends Lexer {
                     this.pendingTokens.push(untabToken);
                 }
             }
-            
+
             this.lastPosition = token.stopIndex + 1;
             return this.pendingTokens.shift()!;
         }
-        
+
         if (token.type === Token.EOF && this.indentStack.length > 1) {
             while (this.indentStack.length > 1) {
                 this.indentStack.pop();
@@ -115,14 +115,26 @@ export class IndentationLexer extends Lexer {
             this.pendingTokens.push(token);
             return this.pendingTokens.shift()!;
         }
-        
+
         return token;
     }
 
-    get channelNames(): string[] { return this.wrapped.channelNames; }
-    get grammarFileName(): string { return this.wrapped.grammarFileName; }
-    get modeNames(): string[] { return this.wrapped.modeNames; }
-    get ruleNames(): string[] { return this.wrapped.ruleNames; }
-    get serializedATN(): string { return this.wrapped.serializedATN; }
-    get vocabulary(): any { return this.wrapped.vocabulary; }
+    get channelNames(): string[] {
+        return this.wrapped.channelNames;
+    }
+    get grammarFileName(): string {
+        return this.wrapped.grammarFileName;
+    }
+    get modeNames(): string[] {
+        return this.wrapped.modeNames;
+    }
+    get ruleNames(): string[] {
+        return this.wrapped.ruleNames;
+    }
+    get serializedATN(): string {
+        return this.wrapped.serializedATN;
+    }
+    get vocabulary(): any {
+        return this.wrapped.vocabulary;
+    }
 }
