3120f65aeccdd340500e81969d601e57ca6d10d1
Valentine <thayornswordsman@gmail.com>
2025-09-02 10:13:33 +0300

    fix(#64): cleaning up unnecessary tests

--- test/indentation-lexer.test.ts
+++ test/indentation-lexer.test.ts
@@ -45,56 +45,6 @@ describe("IndentationLexer", () => {
 
     describe("lookAhead", () => {
 
-        const testCases = [
-            {
-                name: "we expect to get the correct calls of textSpaces method",
-                inputToChrStream: 't\n42 > @\n  bar > foo\n    "hello, world"\n  bar > foo\n',
-                expectedCalls: ["", "  ", "    ", "  ", ""],
-                expectedCount: 5
-            },
-            {
-                name: "we expect to get the correct calls of textSpaces method",
-                inputToChrStream: '\n\n42 > @\n  bar > foo\n    "hello, world"\n  bar > foo\n',
-                expectedCalls: ["", "", "  ", "    ", "  ", ""],
-                expectedCount: 6
-            }
-        ];
-        test.each(testCases)("$name", ({ inputToChrStream, expectedCalls, expectedCount }) => {
-            const chrStream = CharStreams.fromString(inputToChrStream);
-            const lexer = new IndentationLexer(chrStream);
-            const spacesPushSpy = jest.spyOn((lexer as any).spaces, "push");
-            const tokens: Token[] = [];
-            let token: Token = lexer.nextToken();
-            while (token.type !== Token.EOF) {
-                tokens.push(token);
-                token = lexer.nextToken();
-            }
-            expect(tokens.length).toBeGreaterThan(0);
-            expectedCalls.forEach((value, index) => {
-                expect(spacesPushSpy).toHaveBeenNthCalledWith(index + 1, value);
-            });
-            expect(spacesPushSpy).toHaveBeenCalledTimes(expectedCount);
-            spacesPushSpy.mockRestore();
-        });
-
-        test("we expect textSpaces method is called with correct EOL text", () => {
-            const input = "\nbar > foo\n  42 > @\n";
-            const chrStream = CharStreams.fromString(input);
-            const lexer = new IndentationLexer(chrStream);
-            const textSpacesSpy = jest.spyOn(IndentationLexer as any, "textSpaces");
-            const tokens: Token[] = [];
-            let token: Token = lexer.nextToken();
-            while (token.type !== Token.EOF) {
-                tokens.push(token);
-                token = lexer.nextToken();
-            }
-            expect(textSpacesSpy).toHaveBeenCalledWith("\n");
-            expect(textSpacesSpy).toHaveBeenCalledWith("\n  ");
-            expect(textSpacesSpy).toHaveBeenCalledWith("\n");
-            expect(textSpacesSpy).toHaveBeenCalledTimes(3);
-            textSpacesSpy.mockRestore();
-        });
-
         test("we expect token to be EOF in an empty CharStreams", () => {
             const input = "";
             const chrStream = CharStreams.fromString(input);
