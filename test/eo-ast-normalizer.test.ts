f88139b8f2eb404b20a6bf4b295dcca66430b8c0
Valentine <afaga@pop-os.Dlink>
2025-10-19 03:53:31 +0300

    fix(#92): proper tests structure refactor

--- test/eo-ast-normalizer.test.ts
+++ test/eo-ast-normalizer.test.ts
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: MIT
 
 import { EoASTNormalizer, NormalizedNode } from "../src/EoASTNormalizer";
-import { ObjectContext } from "../src/parser/EoParser";
+import { ProgramContext } from "../src/parser/EoParser";
 import { Processor } from "../src/processor";
 import path from "path";
 import * as fs from "fs";
@@ -10,79 +10,86 @@ import * as fs from "fs";
 
 describe("EoASTNormalizer", () => {
     let normalizer: EoASTNormalizer;
-    const programPath = path.join(__dirname, "../fixtures", "fibonacci.eo");
-    const text = fs.readFileSync(programPath, "utf8");
-    const processor = new Processor(text);
-    const programCtx = processor.parser.program();
+    let programCtx: ProgramContext;
+
+    beforeAll(() => {
+        const programPath = path.join(__dirname, "../fixtures", "fibonacci.eo");
+        const text = fs.readFileSync(programPath, "utf8");
+        const processor = new Processor(text);
+        programCtx = processor.parser.program();
+    });
 
     beforeEach(() => {
         normalizer = new EoASTNormalizer();
     });
 
-    test("we expect to obtain an equal nodes", () => {
-        const getSpy = jest.spyOn((normalizer as any).normalizedNodes, "get");
-        const nodes = normalizer.normalizeProgram(programCtx);
-        expect(nodes.length).toBeGreaterThan(0);
-        const nodes2 = normalizer.normalizeProgram(programCtx);
-        expect(getSpy).toHaveBeenCalledTimes(1);
-        expect(nodes2).toEqual(nodes);
-        getSpy.mockRestore();
+    describe("EoASTNormalizer integration tests", () => {
+        test("we expect to obtain an equal nodes", () => {
+            const getSpy = jest.spyOn((normalizer as any).normalizedNodes, "get");
+            const nodes = normalizer.normalizeProgram(programCtx);
+            expect(nodes.length).toBeGreaterThan(0);
+            const nodes2 = normalizer.normalizeProgram(programCtx);
+            expect(getSpy).toHaveBeenCalledTimes(1);
+            expect(nodes2).toEqual(nodes);
+            getSpy.mockRestore();
+        });
+        test("we expect findNestedChildrenRecursively to return 6 nested children", () => {
+            const objectCtx = programCtx.object();
+            if (!objectCtx) {
+                throw new Error("No ObjectContext found in Fibonacci test file");
+            }
+            const result = normalizer.findNestedChildrenRecursively(objectCtx);
+            expect(result.length).toBe(6);
+        });
     });
 
-    test("we expect normalizeBound and normalizeMasterBody to return cached value", () => {
-        const mockCtx = {
-            text: "> cachedAttribute",
-            start: { line: 1, charPositionInLine: 0 },
-            stop: { line: 1, charPositionInLine: 16, text: "> cachedAttribute" }
-        };
-        const cachedValue = {
-            type: "attribute",
-            name: "cachedAttribute",
-            range: { start: { line: 0, character: 0 }, end: { line: 0, character: 16 } },
-            selectionRange: { start: { line: 0, character: 0 }, end: { line: 0, character: 16 } },
-            children: [],
-            text: "> cachedAttribute"
-        };
-        const normalizedNodes = (normalizer as any).normalizedNodes;
-        normalizedNodes.set(mockCtx, cachedValue);
-        const getSpy = jest.spyOn(normalizedNodes, "get");
-        const boundResult = normalizer.normalizeBound(mockCtx as any);
-        const masterBodyResult = normalizer.normalizeMasterBody(mockCtx as any);
-        expect(getSpy).toHaveBeenCalledTimes(2);
-        expect(getSpy).toHaveBeenCalledWith(mockCtx);
-        expect(boundResult).toBe(cachedValue);
-        expect(masterBodyResult).toBe(cachedValue);
-        getSpy.mockRestore();
-    });
+    describe("EoASTNormalizer mock tests", () => {
+        test("we expect normalizeBound and normalizeMasterBody to return cached value", () => {
+            const mockCtx = {
+                text: "> cachedAttribute",
+                start: { line: 1, charPositionInLine: 0 },
+                stop: { line: 1, charPositionInLine: 16, text: "> cachedAttribute" }
+            };
+            const cachedValue = {
+                type: "attribute",
+                name: "cachedAttribute",
+                range: { start: { line: 0, character: 0 }, end: { line: 0, character: 16 } },
+                selectionRange: { start: { line: 0, character: 0 }, end: { line: 0, character: 16 } },
+                children: [],
+                text: "> cachedAttribute"
+            };
+            const normalizedNodes = (normalizer as any).normalizedNodes;
+            normalizedNodes.set(mockCtx, cachedValue);
+            const getSpy = jest.spyOn(normalizedNodes, "get");
+            const boundResult = normalizer.normalizeBound(mockCtx as any);
+            const masterBodyResult = normalizer.normalizeMasterBody(mockCtx as any);
+            expect(getSpy).toHaveBeenCalledTimes(2);
+            expect(getSpy).toHaveBeenCalledWith(mockCtx);
+            expect(boundResult).toBe(cachedValue);
+            expect(masterBodyResult).toBe(cachedValue);
+            getSpy.mockRestore();
+        });
 
-    test("we expect findTopLevelChildren to trigger processSubMaster method", () => {
-        const children: NormalizedNode[] = [];
-        const mockSubMasterContext = {
-            masterBody: jest.fn(),
-            getChild: jest.fn(),
-            childCount: 0,
-            text: "sub-master"
-        };
-        const mockObjectContext = {
-            masterBody: jest.fn().mockReturnValue(null),
-            childCount: 1,
-            getChild: jest.fn().mockReturnValue(mockSubMasterContext)
-        };
-        const originalSubMaster = jest.requireActual("../src/parser/EoParser").SubMasterContext;
-        Object.setPrototypeOf(mockSubMasterContext, originalSubMaster.prototype);
-        const processSubMasterSpy = jest.spyOn((normalizer as any), "processSubMaster");
-        normalizer.findTopLevelChildren(mockObjectContext as any);
-        expect(processSubMasterSpy).toHaveBeenCalled();
-        expect(processSubMasterSpy).toHaveBeenCalledWith(mockSubMasterContext, children);
-        processSubMasterSpy.mockRestore();
-    });
-
-    test("we expect findNestedChildrenRecursively to return 6 nested children", () => {
-        const objectCtx = programCtx.object();
-        if (!objectCtx) {
-            throw new Error("No ObjectContext found in Fibonacci test file");
-        }
-        const result = normalizer.findNestedChildrenRecursively(objectCtx);
-        expect(result.length).toBe(6);
+        test("we expect findTopLevelChildren to trigger processSubMaster method", () => {
+            const children: NormalizedNode[] = [];
+            const mockSubMasterContext = {
+                masterBody: jest.fn(),
+                getChild: jest.fn(),
+                childCount: 0,
+                text: "sub-master"
+            };
+            const mockObjectContext = {
+                masterBody: jest.fn().mockReturnValue(null),
+                childCount: 1,
+                getChild: jest.fn().mockReturnValue(mockSubMasterContext)
+            };
+            const originalSubMaster = jest.requireActual("../src/parser/EoParser").SubMasterContext;
+            Object.setPrototypeOf(mockSubMasterContext, originalSubMaster.prototype);
+            const processSubMasterSpy = jest.spyOn((normalizer as any), "processSubMaster");
+            normalizer.findTopLevelChildren(mockObjectContext as any);
+            expect(processSubMasterSpy).toHaveBeenCalled();
+            expect(processSubMasterSpy).toHaveBeenCalledWith(mockSubMasterContext, children);
+            processSubMasterSpy.mockRestore();
+        });
     });
 });
