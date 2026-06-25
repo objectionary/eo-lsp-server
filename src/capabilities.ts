656d8f07c104fc05a66b4bf84c7defd37b8fa0d2
Valentine Frolov <thayornSwordsMan@gmail.com>
2026-06-25 19:48:36 +0300

    fix(#336): drop four unused client-capability fields and cover the analyzer with tests (#361)
    Co-authored-by: Valentine <valentine@pop-os.Dlink>

--- src/capabilities.ts
+++ src/capabilities.ts
@@ -1,25 +1,16 @@
 // SPDX-FileCopyrightText: Copyright (c) 2024-2026 Objectionary.com
 // SPDX-License-Identifier: MIT
 
-import { ClientCapabilities, SymbolKind, SymbolTag } from "vscode-languageserver";
+import { ClientCapabilities } from "vscode-languageserver";
 
 export class ClientCapabilitiesAnalyzer {
     public readonly hasConfigurationSupport: boolean;
     public readonly hasWorkspaceFolderSupport: boolean;
-    public readonly hasDiagnosticsSupport: boolean;
     public readonly hasTokensSupport: boolean;
-    public readonly hasHierarchicalDocumentSymbolSupport: boolean;
-    public readonly hasSymbolKindsSupport: SymbolKind[] | undefined;
-    public readonly hasSymbolTagsSupport: SymbolTag[] | undefined;
 
     constructor(capabilities: ClientCapabilities) {
         this.hasConfigurationSupport = !!capabilities.workspace?.configuration;
         this.hasWorkspaceFolderSupport = !!capabilities.workspace?.workspaceFolders;
-        this.hasDiagnosticsSupport = !!capabilities.textDocument?.publishDiagnostics?.relatedInformation;
         this.hasTokensSupport = !!capabilities.textDocument?.semanticTokens;
-        const documentSymbolCapabilities = capabilities.textDocument?.documentSymbol;
-        this.hasHierarchicalDocumentSymbolSupport = !!documentSymbolCapabilities?.hierarchicalDocumentSymbolSupport;
-        this.hasSymbolKindsSupport = documentSymbolCapabilities?.symbolKind?.valueSet;
-        this.hasSymbolTagsSupport = documentSymbolCapabilities?.tagSupport?.valueSet;
     }
 }
